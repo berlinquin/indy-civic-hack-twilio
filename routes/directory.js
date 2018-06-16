e3a64aba9d14040c67f9ec11ab19c07f5bed48f7
Quinten Kent <berlinquin@gmail.com>
2018-06-15 23:08:41 -0400

    Basic branching on first contact

--- routes/directory.js
+++ routes/directory.js
@@ -4,8 +4,34 @@ var express = require('express')
   , router = express.Router()
   , twilio = require('twilio')
   , employeeFinder = require('../lib/employee-finder')
+  , userFinder = require('../lib/user-finder')
   , _ =  require('underscore')
   , twimlGenerator = require('../lib/twiml-generator');
+
+
+// POST /directory/test
+router.post('/test/', function(req, res, next) {
+  var body = req.body.Body;
+  var msg = body.toLowerCase().trim()
+  var userPhone = body.From
+  res.type('text/xml');
+
+  // In this case, this is a returning user
+  if (req.cookies.cachedUser !== undefined && !isNaN(body)) {
+    var user = userFinder.findByNumber(userPhone)
+    console.log('found this user: ', user)
+    // var stage = user
+  }
+  // This is a new user
+  else {
+    // if (msg === 'food') {
+    // } else {
+    // }
+    res.cookie('cachedUser', userPhone, { maxAge: 1000 * 60 * 60 });
+    res.send(twimlGenerator.welcome().toString())
+  }
+  // handle case where this is initial conversation
+})
 
 // POST /directory/search/
 router.post('/search/', function(req, res, next) {
